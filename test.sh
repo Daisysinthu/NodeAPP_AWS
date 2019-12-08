150e93b51d41a2e302b8e50969acfc8e154f5569
Daisysinthu <daisydharshini2696@gmail.com>
2019-12-08 14:35:15 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,6 +3,8 @@ SERVICE=hello_world_node_app
 if P=$(ps -aux | grep 8000)
 then
     echo "$SERVICE is running, PID is $P"
+    echo "Executing changes in production Server"
+    curl -X POST http://<Production_Jenkins_URL>:8080/job/Production_Application_job/build
 else
-    echo "$SERVICE is not running"
+    echo "$SERVICE is not running. Please recheck the changes and build again to deploy application to the production" | mail -s "Gogo Hello_world_app is not running" developer@gogo.com
 fi
